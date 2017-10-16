c1881387ca31300256821997350f868869acb68b
Colum Bennett <cbennett@redhat.com>
2017-10-16 20:40:41 +0100

    update home example test

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -1,17 +1,38 @@
-// import { browser, element, by, ElementFinder } from 'protractor';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { HomePage } from "./home";
+import { IonicModule, NavController } from "ionic-angular/index";
 
-// dummy test case, not an example
+describe('HomePage', function () {
+  let de: DebugElement;
+  let comp: HomePage;
+  let fixture: ComponentFixture<HomePage>;
 
-describe('HomePage', () => {
-  // let test:boolean = true;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HomePage ],
+      imports: [
+        IonicModule.forRoot(HomePage)
+      ],
+      providers: [
+        NavController
+      ]
+    });
+  }));
 
   beforeEach(() => {
-    console.log('test')
-    // browser.get('');
+    fixture = TestBed.createComponent(HomePage);
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('h3'));
   });
 
-  it('should be true', function() {
-    let test:boolean = true;
-    expect(test).toBe(true);
+  it('should create component', () => expect(comp).toBeDefined() );
+
+  it('should have expected <h3> text', () => {
+    fixture.detectChanges();
+    const h3 = de.nativeElement;
+    expect(h3.innerText).toMatch(/Ionic/i,
+      '<h3> should say something about "Ionic"');
   });
 });
